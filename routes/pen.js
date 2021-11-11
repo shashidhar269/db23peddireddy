59d8c56a07beb682db19bf25ba4a43b9e7e7ae42
shashidhar269 <shashidharreddy269@gmail.com>
2021-11-10 22:45:35 -0600

    changes made to the pen.js

--- routes/pen.js
+++ routes/pen.js
@@ -1,5 +1,9 @@
 var express = require('express');
+const pen_controlers= require('../controllers/camera');
 var router = express.Router();
+/* GET bottle */
+router.get('/', pen_controlers.pen_view_all_Page );
+module.exports = router;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
